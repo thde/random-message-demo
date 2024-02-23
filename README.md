bce34fff5e6b74b9bfb9353f258d66df0ef092ef
nemat97 <nematollahqasemi@gmail.com>
2024-02-23 13:30:45 +0100

    go script for testing deploio

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # random-message-go
+
+1. create a db with some random messages in it.
+2. write a go script that does the following:
+   1. connect to the databank
+   2. select a random message that gets displayed as a result.
+3. set up the app with the deploio
+   1. push the code to a public repo on deploio
+   2. display the scripts result to a html site
+   3. secure the db's username and password using env-variable in diploio
+
+
+
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:      os.Getenv("DBUSER"),
+		Passwd:    os.Getenv("DBPASS"),
+		Net:       "tcp",
+		Addr:      "mysql-844ee58.3a76d95.mysql.nineapis.ch",
+		DBName:    "app_prod",
+		TLSConfig: "skip-verify",
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	defer db.Close() // Ensure database connection is closed even if there's an error
+
+	// Execute the query to retrieve a row from DB
+	var message string
+	query := "SELECT message FROM random_messages ORDER BY RAND() LIMIT 1;"
+	row := db.QueryRow(query)
+	err = row.Scan(&message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the retrieved message
+	fmt.Println("Daily roasting:", message)
+}
